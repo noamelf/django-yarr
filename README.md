fa001ec2ff0f07bd55a09f6de9b9a6fa9f2d9124
shirelf <58177758+shirelf@users.noreply.github.com>
2020-12-17 14:54:48 +0200

    print author name for each article in the page: print the link,title,date

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ### Step 1
 
-1. Fetch the article page for a single Haaretz writer. Print the title, description, writer and date
-2. Try to do the same for other writers as well
+1. ~Fetch the article page for a single Haaretz writer. Print the title, description, writer and date~
+2. ~Try to do the same for other writers as well~
 3. Take the data that was printed earlier and use [Feedgen](https://github.com/lkiesow/python-feedgen) to create an RSS feed ([example](https://www.w3schools.com/xml/xml_rss.asp))
 3. Final results: create a program that receives an Haaretz writer URL from stdin and in return prints the RSS result
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+from datetime import datetime
 
 from bs4 import BeautifulSoup
 
 import requests as req
 from feedgen.feed import FeedGenerator
 
-resp = req.get('https://www.haaretz.co.il/misc/writers/WRITER-1.681400')
+resp = req.get('https://www.haaretz.co.il/misc/writers/WRITER-1.850')
 
 soup = BeautifulSoup(resp.text, 'lxml')
 
+author= soup.find_all('section')[0].find_all('div')[2].text
+print(author)
 for elem in soup.find_all('article'):
     link = elem.find_all('a')[0].attrs['href']
     print(link)
@@ -16,6 +19,7 @@ for elem in soup.find_all('article'):
         title = elem.find_all('a')[1].text
     print(title)
     if elem.find('time'):
-        date=elem.find('time').attrs['datetime']
+        date = elem.find('time').attrs['datetime']
         print(date)
-
+    else:
+        print(datetime.now())
