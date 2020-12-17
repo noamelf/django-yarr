c05a3d7f9ac0b7bae273304cc4f0bf2faed350d7
shirelf <58177758+shirelf@users.noreply.github.com>
2020-12-17 14:13:29 +0200

    for each article in the page: print the link,title,date

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from bs4 import BeautifulSoup
+
 import requests as req
 from feedgen.feed import FeedGenerator
 
@@ -7,30 +8,14 @@ resp = req.get('https://www.haaretz.co.il/misc/writers/WRITER-1.681400')
 
 soup = BeautifulSoup(resp.text, 'lxml')
 
-#print(soup.title)
-#subtitle
-print(soup.select_one('#pageRoot > section.n.o.nd > div > article.gp.ns.as.n.ac.nt.nu.nv.nw.nx.ny.nz.oa.ob.oc.od > div.oi.oj.ok.ol.om.on.oo.op.oq').text)
-#time
-print(soup.select_one('#pageRoot > section.n.o.nd > div > article.gp.ns.as.n.ac.nt.nu.nv.nw.nx.ny.nz.oa.ob.oc.od > footer > time').text)
-#author
-print(soup.select_one('#pageRoot > section.n.o.nd > div > article.gp.ns.as.n.ac.nt.nu.nv.nw.nx.ny.nz.oa.ob.oc.od > footer > span.oz').text)
-#image
-print(soup.select_one('#pageRoot > section.n.o.nd > div > article.gp.ns.as.n.ac.nt.nu.nv.nw.nx.ny.nz.oa.ob.oc.od > div.oe.as.of').getText)
-author =soup.select_one('#pageRoot > section.n.o.nd > div > article.gp.ns.as.n.ac.nt.nu.nv.nw.nx.ny.nz.oa.ob.oc.od > footer > span.oz').text
+for elem in soup.find_all('article'):
+    link = elem.find_all('a')[0].attrs['href']
+    print(link)
+    title = elem.find_all('a')[0].text
+    if not title:
+        title = elem.find_all('a')[1].text
+    print(title)
+    if elem.find('time'):
+        date=elem.find('time').attrs['datetime']
+        print(date)
 
-
-
-
-
-fg = FeedGenerator()
-fg.id('https://www.haaretz.co.il/misc/writers/WRITER-1.681400')
-fg.title(soup.select_one('#pageRoot > section.n.o.nd > div > article.gp.ns.as.n.ac.nt.nu.nv.nw.nx.ny.nz.oa.ob.oc.od > div.oi.oj.ok.ol.om.on.oo.op.oq').text)
-fg.author({'name': author})
-#fg.link(href='http://example.com', rel='alternate')
-fg.logo(soup.select_one('#pageRoot > section.n.o.nd > div > article.gp.ns.as.n.ac.nt.nu.nv.nw.nx.ny.nz.oa.ob.oc.od > div.oe.as.of').getText)
-fg.subtitle(soup.select_one('#pageRoot > section.n.o.nd > div > article.gp.ns.as.n.ac.nt.nu.nv.nw.nx.ny.nz.oa.ob.oc.od > footer > time').text)
-#fg.link(href='http://larskiesow.de/test.atom', rel='self')
-#fg.language('en')
-
-rssfeed= fg.rss_str(pretty=True)# Get the RSS feed as string
-fg.rss_file("rss.xml")# Write the RSS feed to a file
