83e836fd083fa57009c1c5b34d5981a5370aa666
Noam Elfanbaum <noam.elf@gmail.com>
2020-12-26 10:39:12 +0200

    Added login

--- Makefile
+++ Makefile
@@ -1,8 +1,18 @@
-all: migrate check-feeds serve
+refresh: down up
+
+up: serve sleep migrate check-feeds 
+
+down:
+	docker-compose down
+
+sleep:
+	sleep 2
 
 migrate:
 	docker-compose run mywriters migrate
+
 check-feeds:
 	docker-compose run mywriters check-feeds
+
 serve:
 	docker-compose up -d
--- mywriters/mywriters/views.py
+++ mywriters/mywriters/views.py
@@ -1,6 +1,23 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
 
 def index(request):
-    context={'message': 'Welcome!', 'feed_url': '/yarr'}
+    context = {'message': 'Welcome!', 'feed_url': '/yarr'}
     return render(request, 'index.html', context)
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
