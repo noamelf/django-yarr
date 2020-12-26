d7c99905efe7846c06715ae9897a23c7949d919a
Noam Elfanbaum <noam.elf@gmail.com>
2020-12-26 08:59:32 +0200

    Change structure

--- ui/mywriters/mywriters/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def index(request):
-    context={'message': 'Welcome!', 'feed_url': '/yarr'}
-    return render(request, 'index.html', context)
