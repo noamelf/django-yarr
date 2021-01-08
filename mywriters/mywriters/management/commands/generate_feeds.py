ac19c2c24dbacdadc3d6d6ae03015199d27f1a62
Noam Elfanbaum <noam.elf@gmail.com>
2021-01-09 00:31:00 +0200

    Log duplicate URLs

--- mywriters/mywriters/management/commands/generate_feeds.py
+++ mywriters/mywriters/management/commands/generate_feeds.py
@@ -1,6 +1,7 @@
 from logging import getLogger
 
 from django.core.management.base import BaseCommand
+from django.db.utils import IntegrityError
 from mywriters.generate_feeds import haaretz
 from yarr.models import Feed
 
@@ -13,8 +14,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for name, image, link, rss in haaretz.create_haaretz_feeds():
             self.stdout.write(f"Fetching {name}")
-            Feed.objects.update_or_create(
-                title=name, site_url=link, defaults={"rss": rss, "image": image}
-            )
+            try:
+                Feed.objects.update_or_create(
+                    title=name, site_url=link, defaults={"rss": rss, "image": image}
+                )
+            except IntegrityError as e:
+                self.stdout.write(e)
 
         self.stdout.write(self.style.SUCCESS("Successfully generated feeds"))
