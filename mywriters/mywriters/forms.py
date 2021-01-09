0683f622fc1713df19d2904c497d6dcd70980fc0
Noam Elfanbaum <noam.elf@gmail.com>
2021-01-09 18:17:35 +0200

    Flattend structure

--- mywriters/mywriters/forms.py
+++
@@ -1,9 +0,0 @@
-from mywriters.models import UserFeeds
-from django import forms
-
-
-
-class AddFeedForm(forms.ModelForm):
-    class Meta:
-        model = UserFeeds
-        fields = ["feeds"]
--- mywriters/mywriters/models.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings as django_settings
-from django.db import models
-from yarr.models import Feed
-
-
-class UserFeeds(models.Model):
-    user = models.ForeignKey(django_settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    feeds = models.ManyToManyField(Feed)
